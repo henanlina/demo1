fb4f6a56ea93d218eb0ba08191a224fbb0ede5e7
xjj1987@qq.com <xjj1987@qq.com>
2017-08-02 14:09:29 +0800

    以post方式发送json编码数据请求接口

---
+++ sms.php
@@ -0,0 +1,36 @@
+<?php
+
+$mobile = $_GET['mobile'];
+$content = $_GET['content'];
+$url = 'http://10.10.32.2:8080/service/data/ws/rest/message/checkAndSend';
+$post_data = array(
+    'content'=>$content,
+    'productType'=>'kkd',
+    'mobile'=>$mobile
+);
+
+$res = http_post_json($url,json_encode($post_data));
+/**
+ * PHP发送Json对象数据, 发送HTTP请求
+ *
+ * @param string $url 请求地址
+ * @param array $data 发送数据
+ * @return String
+ */
+function http_post_json($url, $data) {
+    $ch = curl_init ( $url );
+    curl_setopt ( $ch, CURLOPT_POST, 1 );
+    curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+    curl_setopt ( $ch, CURLOPT_FRESH_CONNECT, 1 );
+    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt ( $ch, CURLOPT_FORBID_REUSE, 1 );
+    curl_setopt ( $ch, CURLOPT_TIMEOUT, 30 );
+    curl_setopt ( $ch, CURLOPT_HTTPHEADER, array ('Content-Type: application/json; charset=utf-8', 'Content-Length: ' . strlen ($data) ) );
+    curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+    $ret = curl_exec ( $ch );
+    echo  "Request Info : url: " . $url . " ,send data: " . $data . "  \n";
+    echo  "Respnse Info : " . $ret . "  \n";
+    curl_close ( $ch );
+    return $ret;
+}
+
