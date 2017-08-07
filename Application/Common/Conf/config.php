8dd7f1c4ce038e1d796bfda8477b7370399ba019
lina <1427033272@qq.com>
2017-08-07 18:03:03 +0800

    默认指定模块

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,4 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
     'STATIC_URL' =>'http://www.demo.com',
+    //设置允许访问的模块
+    'MODULE_ALLOW_LIST' => ['Home','Admin'],
+    //设置默认模块
+    'DEFAULT_MODULE' => 'Home',
 );
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,24 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Common\ORG\Util\MySendMail;
 class IndexController extends Controller {
     public function index(){
         $this->display();
+    }
+
+    public function test(){
+        $mail = new MySendMail();
+        $mail->setServer("127.0.0.1", "admin", "gaoxioalv");
+        $mail->setFrom("15838350795@163.com");
+        $mail->setReceiver("1427033272@qq.com");
+//$mail->setReceiver("XXXXX@XXXXX");
+//        $mail->setCc("XXXXX@XXXXX");
+//        $mail->setCc("XXXXX@XXXXX");
+//        $mail->setBcc("XXXXX@XXXXX");
+//        $mail->setBcc("XXXXX@XXXXX");
+//        $mail->setBcc("XXXXX@XXXXX");
+        $mail->setMailInfo("test", "<b>test</b>", "sms.zip");
+        $mail->sendMail();
     }
 }
