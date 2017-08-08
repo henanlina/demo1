8590c93e83cbd9dfbb74b4e37dc10785ab700a1a
lina <1427033272@qq.com>
2017-08-08 10:31:49 +0800

    PHP不搭建SMTP本地服务器发送邮件

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,15 +9,23 @@ class IndexController extends Controller {
 
     public function test(){
         $mail = new MySendMail();
-        $mail->setServer("127.0.0.1", "admin", "gaoxioalv");
+        /**
+         * 使用163邮箱需要开启用户授权码，不开启授权发送不成功
+         */
+        $mail->setServer("smtp.163.com", "15838350795@163.com", "LiGx3375Nav");
+        //发送人
         $mail->setFrom("15838350795@163.com");
+        //接收人
         $mail->setReceiver("1427033272@qq.com");
-//$mail->setReceiver("XXXXX@XXXXX");
-//        $mail->setCc("XXXXX@XXXXX");
-//        $mail->setCc("XXXXX@XXXXX");
-//        $mail->setBcc("XXXXX@XXXXX");
-//        $mail->setBcc("XXXXX@XXXXX");
-//        $mail->setBcc("XXXXX@XXXXX");
+        //接收人可以是多个
+        //$mail->setReceiver("XXXXX@XXXXX");
+        //抄送人
+        //$mail->setCc("XXXXX@XXXXX");
+        //$mail->setCc("XXXXX@XXXXX");
+        //秘密抄送人
+        //$mail->setBcc("XXXXX@XXXXX");
+        //$mail->setBcc("XXXXX@XXXXX");
+        //$mail->setBcc("XXXXX@XXXXX");
         $mail->setMailInfo("test", "<b>test</b>", "sms.zip");
         $mail->sendMail();
     }
