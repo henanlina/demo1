d3adac344b951e5c7e307f831a3f75c0ca47a201
lina <1427033272@qq.com>
2017-08-21 17:06:05 +0800

    定制成功失败页面

---
+++ Application/Common/Common/format.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by lina.
+ * User: admin
+ * Date: 2017/8/17
+ * Time: 15:22
+ */
+/**
+ * 格式化参数
+ */
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,4 +6,8 @@ return array(
     'MODULE_ALLOW_LIST' => ['Home','Admin'],
     //设置默认模块
     'DEFAULT_MODULE' => 'Home',
+    //自定义错误模板
+    'TMPL_ACTION_ERROR' =>'./Public/error.html',
+    //自定义成功模板
+    'TMPL_ACTION_SUCCESS' =>'./Public/success.html',
 );
